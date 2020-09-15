8f4f630ed0872c140c1746175ca437abb140bec1
Tilson Beattie <BeatTils377@GoTVDSB.ca>
2020-09-15 13:33:04 -0400

    ewest changes

--- part 2 VS/Program.cs
+++ part 2 VS/Program.cs
@@ -6,8 +6,142 @@ namespace part_2_VS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //int a = 18;
+            //int b = 6;
+            //int c = a + b;
+
+            //Console.WriteLine(c);
+
+            //c = a - b;
+            //Console.WriteLine(c);
+
+            //c = a * b;
+            //Console.WriteLine(c);
+
+
+            //c = a / b;
+            //Console.WriteLine(c);
+
+
+            //int a = 18;
+            //int b = 6;
+
+            //int c = a + b;
+            //Console.WriteLine(c);
+
+            //c = a - b;
+            //Console.WriteLine(c);
+
+            //c = a / b;
+            //Console.WriteLine(c);
+
+            //int a = 5;
+            //int b = 4;
+            //int c = 2;
+            //int d = a + b * c;
+            //Console.WriteLine(d);
+
+            //d = (a + b) * c;
+            //Console.WriteLine(d);
+
+
+
+            //d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            //Console.WriteLine(d);
+
+
+            //int e = 7;
+            //int f = 4;
+            //int g = 3;
+            //int h = (e + f) / g;
+            //Console.WriteLine(h);
+
+
+
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
-asda
