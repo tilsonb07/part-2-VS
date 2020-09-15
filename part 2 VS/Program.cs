a9e4ba5b795b9be0610cd61e9198ba32ef33f09e
Tilson Beattie <BeatTils377@GoTVDSB.ca>
2020-09-15 11:53:28 -0400

    Add project files.

---
+++ part 2 VS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace part_2_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
